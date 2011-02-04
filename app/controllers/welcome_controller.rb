43ba5b406abe2b667d521b069e0ce355d3859bb0
Justin Beck <justin.beck@meddius.com>
2011-02-04 12:30:52 -0500

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    # This space intentionally left blank
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssPlayground::Application.config.secret_token = '4cb4dd90cdb4eeae612791e850ced20548fd0d677dc3dc3b00ba68f5c3fe0c366b6349ce4aa93ec8925123dec64fbce4c30b4a7f0c9eb77c9a8392abcb2b47b2'
